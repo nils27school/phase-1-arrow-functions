782e48b33e318ae8a9e1d533932b881d8a477f08
nilssvensson <nils27school@gmail.com>
2022-09-01 11:14:13 -0400

    commit tree 92c118e2b4aff062fbf3c2acaa4d178581b50873

---
+++ index.js
@@ -0,0 +1,25 @@
+// function divide() {
+//     let a = 2000
+//     let b = 100
+//     return (a / b);
+// }
+
+// function square() {
+//     let x = 2
+//     return (x = 4);
+// }
+
+// function add() {
+//     let a = 3
+//     let b = 4
+//     return (a + b);
+// }
+
+const divide =  (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000,100)
+
+const square = (x) => x * x;
+square(2)
+
+const add = (a, b) => a + b;
+add(3, 4)
